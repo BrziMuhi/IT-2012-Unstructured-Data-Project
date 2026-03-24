94fab29ead660ff29db123d92fbec71aa4335e70
Muhamed Sekic <muhamed.sekic@stu.ibu.edu.ba>
2026-03-24 01:09:00 +0100

    lab 3

--- src/pipeline/run_pipeline.py
+++ src/pipeline/run_pipeline.py
@@ -6,17 +6,20 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 from utils.logger import logging
 from api.client import fetch_places
 from storage.mongo import save_to_mongo
-from storage.s3 import upload_file_to_s3
+from storage.s3 import upload_file_to_s3            
 
 
 def run_pipeline():
     logging.info("Pipeline started")
 
+    logging.info("Fetching API data...")
     places = fetch_places(3)
 
+    logging.info("Saving data to MongoDB...")
     for place in places:
         save_to_mongo(place, "geoapify_api")
 
+    logging.info("Uploading raw JSON files to S3...")
     for page in range(1, 4):
         file_path = f"data/raw/api/page_{page}.json"
         file_name = f"page_{page}.json"
